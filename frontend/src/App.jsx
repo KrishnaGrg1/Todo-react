ac45437db5b4e4b4f1832fe2e5eb747a6dccc7cf
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-01-31 10:38:55 +0545

    updated the .jsx fule

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { CreateTodo } from "./components/createTodo.jsx";
-import { Todos } from "./components/Todos.jsx";
+import  CreateTodo from "./components/createTodo.jsx";
+import  Todos  from "./components/Todos.jsx";
 
 function App() {
   const [todos, setTodos] = useState([]);
